0c63de68dfe2d181a497c45392d4912f521b3e08
Yasuko Miyazawa <yasukomiyazawa08@gmail.com>
2023-07-24 01:01:45 +0900

    Did the search form and home section. Also worked on responsiveness for both.

--- script.js
+++ script.js
@@ -14,10 +14,36 @@ document.addEventListener('DOMContentLoaded', () => {
       navbar.classList.remove("active");
     };
 
+
+    let searchForm = document.querySelector(".search-form");
+  
+    document.querySelector('#search-button').onclick = () => {
+        console.log("menu-btn clicked")
+      searchForm.classList.add("active");
+    };
+  
+    document.querySelector('#close-search').onclick = () => {
+        console.log("close-btn clicked")
+      searchForm.classList.remove("active");
+    };
+
+
     window.onscroll = () => {
 
         navbar.classList.remove("active")
 
     }
-  });
+
+    var swiper = new Swiper(".home-slider", {
+        loop: true,
+        grabCursor: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+    });
+});
+
+
+  
   
