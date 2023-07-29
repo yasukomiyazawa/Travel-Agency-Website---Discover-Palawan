60b8698c73dae511801da07944ebb8e1e40a350a
Yasuko Miyazawa <yasukomiyazawa08@gmail.com>
2023-07-29 23:43:23 +0900

    added the reviews and blogs section. Also made both responsive.

--- script.js
+++ script.js
@@ -98,6 +98,49 @@ document.addEventListener('DOMContentLoaded', () => {
         },
       },
   });
+
+  var swiper = new Swiper(".review-sliders", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
+  var swiper = new Swiper(".blogs-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+
+    },
+  });
 });
 
 
