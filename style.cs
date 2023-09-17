3c9740d4e647d18667a6d654604a7b82e12c80fe
Yasuko Miyazawa <yasukomiyazawa08@gmail.com>
2023-09-18 02:22:10 +0900

    added footer section

--- style.cs
+++ style.cs
@@ -295,12 +295,14 @@ section {
 }
 .about-content button:hover {
   background: rgb(151, 30, 30);
-  color: #fff;
+  color: #fff !important;
 }
 
 .shop .slide {
   width: 35rem;
   border: 0.2rem solid #10221b;
+  /* Initially hide the icons with opacity 0 and transform off the screen */
+  /* Show the icons when the parent container is hovered */
 }
 .shop .slide .image {
   position: relative;
@@ -315,11 +317,19 @@ section {
   object-fit: cover;
 }
 .shop .slide .hidden-icons {
-  display: none;
+  opacity: 0;
+  transform: translateY(-100%);
+  transition: opacity 0.3s ease, transform 0.3s ease;
+  width: 100%;
+  text-align: center;
+  z-index: 10;
 }
 .shop .slide .image-container:hover .hidden-icons {
   opacity: 1;
   transform: translateY(0);
+  width: 100%;
+  text-align: center;
+  z-index: 10;
 }
 .shop .slide a {
   height: 4.5rem;
@@ -354,18 +364,333 @@ section {
   color: #10221b;
 }
 
+.packages {
+  background: #eee;
+}
+.packages .box-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(32rem, 1fr));
+  gap: 1.5rem;
+}
+.packages .box-container .box {
+  text-align: center;
+  background: #fff;
+  border: 0.2rem solid #10221b;
+  box-shadow: 0 0.5rem 1rem regb(0, 0, 0, 0.1);
+  border-radius: 0.5rem;
+}
+.packages .box-container .box:hover {
+  background: rgb(0, 0, 77);
+}
+.packages .box-container .box:hover .content > * {
+  color: #fff;
+}
+.packages .box-container .box:hover .btn {
+  border: 0.2rem solid #fff;
+}
+.packages .box-container .box .image {
+  height: 25rem;
+  overflow: hidden;
+  padding: 2rem;
+  padding-bottom: 0;
+  border-radius: 0.5rem;
+}
+.packages .box-container .box .image img {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  border-radius: 0.5rem;
+}
+.packages .box-container .box .content {
+  padding: 2rem;
+}
+.packages .box-container .box .content h3 {
+  font-size: 2.5rem;
+  color: #10221b;
+}
+.packages .box-container .box .content p {
+  font-size: 1.5rem;
+  color: #10221b;
+  padding: 1rem 0;
+  line-height: 2;
+}
+.packages .box-container .box .content .price {
+  font-size: 2rem;
+  color: rgb(151, 30, 30);
+}
+.packages .box-container .box .btn {
+  margin-top: 1rem;
+  display: inline-block;
+  border: 0.2rem solid #10221b;
+  color: #10221b;
+  cursor: pointer;
+  background: none;
+  font-size: 1.7rem;
+  padding: 1rem 4rem;
+}
+.packages .box-container .box .btn:hover {
+  background: rgb(242, 206, 0);
+  color: #fff !important;
+}
+
+.services {
+  background: #eee;
+}
+.services .box-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(31rem, 1fr));
+  gap: 1.5rem;
+}
+.services .box-container .box {
+  text-align: center;
+  padding: 2rem;
+  border: 0.2rem solid #10221b;
+  border-radius: 0.5rem;
+  background: #fff;
+}
+.services .box-container .box:hover {
+  background: rgb(151, 30, 30);
+}
+.services .box-container .box:hover img {
+  filter: invert(1);
+}
+.services .box-container .box:hover h3 {
+  color: rgb(242, 206, 0);
+}
+.services .box-container .box:hover p {
+  color: #fff;
+}
+.services .box-container .box:hover .btn {
+  border-color: #fff;
+  color: #fff;
+}
+.services .box-container .box:hover .btn:hover {
+  background: rgb(0, 0, 77);
+}
+.services .box-container .box img {
+  height: 10rem;
+  margin-bottom: 1rem;
+}
+.services .box-container .box h3 {
+  font-size: 2rem;
+  color: rgb(0, 0, 77);
+}
+.services .box-container .box p {
+  font-size: 1.5rem;
+  color: #10221b;
+  padding: 1rem 0;
+  line-height: 2;
+}
+.services .box-container .box .btn {
+  margin-top: 1rem;
+  display: inline-block;
+  border: 0.2rem solid #10221b;
+  color: #10221b;
+  cursor: pointer;
+  background: none;
+  font-size: 1.7rem;
+  padding: 1rem 4rem;
+}
+.services .box-container .box .btn:hover {
+  background: rgb(151, 30, 30);
+  color: #fff !important;
+}
+
+.reviews .slide .text {
+  padding: 2rem;
+  font-size: 1.6rem;
+  font-style: italic;
+  background: #eee;
+  border-radius: 0.5rem;
+  color: #10221b;
+  line-height: 2;
+  position: relative;
+  z-index: 0;
+  margin: 3rem 0;
+}
+.reviews .slide .text::before {
+  content: "";
+  position: absolute;
+  bottom: -1.5rem;
+  left: 0.7rem;
+  height: 3rem;
+  width: 3rem;
+  background-color: #eee;
+  transform: rotate(45deg);
+}
+.reviews .slide .user {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+.reviews .slide .user .review-picture {
+  border: 0.2rem solid #10221b;
+  height: 5rem;
+  width: 5rem;
+  border-radius: 50%;
+  padding: 2rem;
+  display: inline-block;
+  background: rgb(151, 30, 30);
+}
+.reviews .slide .user h3 {
+  font-size: 2rem;
+  color: rgb(0, 0, 77);
+}
+.reviews .slide .user .stars-review i {
+  color: rgb(242, 206, 0);
+  font-size: 1.5rem;
+}
+
+.blogs .slide {
+  text-align: center;
+  padding: 2rem;
+}
+.blogs .slide img {
+  height: 25rem;
+  width: 100%;
+  object-fit: cover;
+  border-radius: 0.5rem;
+}
+.blogs .slide .icons {
+  background: #10221b;
+  border-radius: 0.5rem;
+  padding: 1rem;
+  position: relative;
+  top: -2rem;
+  display: inline-block;
+}
+.blogs .slide .icons .date-writer {
+  font-size: 1.4rem;
+  color: #fff;
+  margin: 0 1rem;
+}
+.blogs .slide .icons .date-writer:hover {
+  color: rgb(242, 206, 0);
+}
+.blogs .slide .icons .date-writer i {
+  padding-right: 0.5rem;
+  color: rgb(242, 206, 0);
+}
+.blogs .slide .btn {
+  margin-top: 1rem;
+  display: inline-block;
+  border: 0.2rem solid #10221b;
+  color: #10221b;
+  cursor: pointer;
+  background: none;
+  font-size: 1.5rem;
+  padding: 1rem 2rem;
+}
+.blogs .slide .btn:hover {
+  background: rgb(151, 30, 30);
+  color: #fff !important;
+}
+.blogs .slide h3 {
+  font-size: 2rem;
+  color: #10221b;
+}
+.blogs .slide p {
+  font-size: 1.4rem;
+  padding: 1rem 0;
+  line-height: 2;
+  color: #10221b;
+}
+
+.newsletter {
+  background: #eee;
+}
+.newsletter .content {
+  max-width: 70rem;
+  margin: 1rem auto;
+  text-align: center;
+}
+.newsletter .content p {
+  font-size: 1.5rem;
+  line-height: 2;
+  color: #10221b;
+}
+.newsletter .content form {
+  margin-top: 2rem;
+  background: #fff;
+  border-radius: 5rem;
+  border: 0.2rem solid #10221b;
+  padding: 0.7rem;
+  display: flex;
+}
+.newsletter .content form .email {
+  width: 100%;
+  background: none;
+  text-transform: none;
+  font-size: 1.6rem;
+  color: #10221b;
+  padding: 0 1.3rem;
+}
+.newsletter .btn {
+  margin-top: 0;
+  display: inline-block;
+  border: 0.2rem solid #10221b;
+  border-radius: 5rem;
+  color: #fff;
+  cursor: pointer;
+  background: #10221b;
+  font-size: 1.5rem;
+  padding: 1rem 2rem;
+}
+.newsletter .btn:hover {
+  background: none;
+  color: #10221b;
+}
+
+.partners .slide {
+  text-align: center;
+}
+.partners .slide img {
+  height: 8rem;
+  width: 200px;
+}
+
+.footer {
+  background-color: black;
+}
+.footer .box-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+  gap: 2rem;
+}
+.footer .box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.footer .box h3, .footer .box a, .footer .box i {
+  color: white;
+}
+.footer .box h3 {
+  font-size: 2rem;
+  padding: 1.5rem 0;
+}
+.footer .box a {
+  display: block;
+  font-size: 1.4rem;
+  padding: 1rem 0;
+}
+.footer .box i {
+  padding-right: 0.5rem;
+}
+.footer .box i:hover {
+  color: rgb(242, 206, 0);
+}
+
 @media (max-width: 1440px) {
   section {
     padding: 3rem 2rem;
   }
 }
-@media (max-width: 1061px) {
+@media (max-width: 1200px) {
   html {
     font-size: 55%;
     scroll-padding-top: 7rem;
   }
-}
-@media (max-width: 954px) {
   .header .icons {
     right: 2rem;
   }
@@ -430,6 +755,9 @@ section {
   }
   .logo {
     font-size: 1.5rem !important;
+  }
+  .heading {
+    font-size: 3rem;
   }
 }
 
